a51fbbac13084d450d47cd7f83f20ac019681092
26zahra <zahra.abdulrahimova2021@khazar.org>
2023-11-08 12:04:31 +0400

    Eco website creating

---
+++ assets/js/eco.js
@@ -0,0 +1,98 @@
+var search = document.querySelectorAll(".nav-button i")[0]
+console.log(search);
+search.addEventListener("click", function (e) {
+    document.querySelector(".modal").style.display = "flex";
+
+})
+var modal = document.querySelector(".modal");
+modal.addEventListener("click", function (e) {
+    console.log(e.target);
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
+})
+
+
+
+var aside = document.querySelector("aside ");
+var icon = document.querySelector("aside i");
+var sidebar = document.querySelectorAll(".nav-button i")[1];
+sidebar.addEventListener("click", function (e) {
+    aside.style.transform = "translateX(0)";
+})
+icon.addEventListener("click", function () {
+    aside.style.transform = "translateX(100%)";
+})
+
+
+
+var navbar = document.querySelector(".navbar");
+// var headerIcons = document.querySelector(".header");
+var eded = document.querySelectorAll(".num-1 h1 span");
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 100) {
+        navbar.classList.add("teze");
+    }
+    else {
+        navbar.classList.remove("teze");
+
+    }
+
+
+})
+
+
+var stop1 = false;
+var eded = document.querySelectorAll(".num-1 h1 span");
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 2100 && !stop1) {
+    
+        eded.forEach(item => {
+            var reqem = Number(item.getAttribute("zehra"));
+            var duration = 1000;
+            var step = reqem / (duration/10);
+            var current = 0;
+            var artim = setInterval(function () {
+                current += step;
+                if (current >= reqem) {
+                    item.innerText = Math.round(current);
+                    clearInterval(artim);
+                } else {
+                    item.innerText = Math.round(current);
+                }
+            }, 10)
+        });
+        stop1 = true;
+    }
+
+})
+
+
+        var slides = document.querySelectorAll(".mySlides");
+        let slideIndex = 0;
+        console.log(slides.length);
+        function next(){
+          if(slideIndex>=slides.length-1){
+            slideIndex=0
+          }else{
+            slideIndex++
+          }
+          showSlides()
+        }
+        function prev(){
+          if(slideIndex<=0){
+            slideIndex=slides.length-1
+          }else{
+            slideIndex--
+          }
+          showSlides()
+        }
+        function showSlides() {
+          for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+          }
+          slides[slideIndex].style.display = "block";  
+        }
+        showSlides();
+
+        
